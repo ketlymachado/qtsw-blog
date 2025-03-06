a4f034d03df00eacb06fca0e1c689372e7333e17
Kétly Gonçalves Machado <ketly.machado@gmail.com>
2025-03-05 21:20:18 -0300

    feat: add static analysis tool (#4)
    * feat: add eslint
    
    * feat: add stylistic

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {
+    plugins: {
+      '@stylistic/ts': stylisticTs
+    },
+    languageOptions: { globals: globals.browser },
+    rules: {
+      '@stylistic/ts/semi': ["error", "always"],
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
