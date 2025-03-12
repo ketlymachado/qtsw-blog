757db18a32bc76c6c2abb2f1d642571fe5ec1acf
GABRIEL DE OLIVEIRA RODRIGUES <gabriel_o_rodrigues@estudante.sc.senai.br>
2025-03-12 20:42:34 -0300

    feat(eslint): adiciona eslint-plugin-import para gerenciar importações

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import stylisticTs from '@stylistic/eslint-plugin-ts';
-
+import stylisticTs from "@stylistic/eslint-plugin-ts";
+import pluginImport from "eslint-plugin-import";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {
     plugins: {
-      '@stylistic/ts': stylisticTs
+      '@stylistic/ts': stylisticTs,
+      import: pluginImport,
     },
     languageOptions: { globals: globals.browser },
     rules: {
       '@stylistic/ts/semi': ["error", "always"],
+      "import/named": "error",
+      "import/namespace": "error",
+      "import/default": "error",
+      "import/export": "error",
     }
   },
   pluginJs.configs.recommended,
