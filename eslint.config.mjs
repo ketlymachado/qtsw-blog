07fe944a2b43ebc67793abe6bf67c1dd984e6605
vinicerutti1 <vinicerutti0@gmail.com>
2025-03-12 19:50:28 -0300

    Foi adicionado uma nova regra para não permitir o uso do console e um novo arquivo de testes dessa regra

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,17 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import stylisticTs from '@stylistic/eslint-plugin-ts';
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
   {
     plugins: {
       '@stylistic/ts': stylisticTs
     },
     languageOptions: { globals: globals.browser },
     rules: {
-      '@stylistic/ts/semi': ["error", "always"],
+      '@stylistic/ts/semi': ["error", "always"],  // Garante ponto e vírgula obrigatório
+      "no-console": ["error", { allow: ["warn", "error"] }]  // Proíbe console.log(), mas permite console.warn() e console.error()
     }
   },
   pluginJs.configs.recommended,
