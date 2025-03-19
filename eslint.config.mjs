be958fff7fe487f11c3ec13aa27e66849dae133e
José Roberto da Silva <114114093+jose-devops@users.noreply.github.com>
2025-03-19 19:24:46 -0300

    Feat: adiciona regra de formatação (#16)
    ok

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,10 @@ export default [
     languageOptions: { globals: globals.browser },
     rules: {
       '@stylistic/ts/semi': ["error", "always"],
+      "@stylistic/ts/quotes": ["error", "double"],
+      "@stylistic/ts/indent": ["error", 2],
+      "@stylistic/ts/comma-dangle": ["error", "always-multiline"],
+
       'no-empty-function': "off",
       '@typescript-eslint/no-empty-function': "error",
       '@typescript-eslint/no-use-before-define': "error",
@@ -23,6 +27,7 @@ export default [
         'warn',
         { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
       ],
+
     }
   },
   pluginJs.configs.recommended,
