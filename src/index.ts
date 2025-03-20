7f956ab09fdb7d8554cbae3927050ab43c89d6c1
vinicerutti1 <vinicerutti0@gmail.com>
2025-03-19 21:52:43 -0300

    Adiciona Prettier ao projeto e configura regras de formatação

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-import path from "path";
+import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import path from 'path';
 
 const prisma = new PrismaClient();
 const app = express();
@@ -8,76 +8,78 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get("/posts", async (req, res) => {
-	try {
-		const posts = await prisma.post.findMany();
-		res.json(posts);
-	} catch (error) {
-		const internalServerError = 500;
-		res.status(internalServerError).json({ error });
-	}
+app.get('/posts', async (req, res) => {
+  try {
+    const posts = await prisma.post.findMany();
+    res.json(posts);
+  } catch (error) {
+    const internalServerError = 500;
+    res.status(internalServerError).json({ error });
+  }
 });
 
 /**este metodo cria um post */
-app.post("/posts", async (req, res) => {
-	const content = req.body.content;
+app.post('/posts', async (req, res) => {
+  const content = req.body.content;
 
-	if (!content) {
-		const BadRequest = 400;
-		res.status(BadRequest).json({ error: "Content is required" });
-		return;
-	}
+  if (!content) {
+    const BadRequest = 400;
+    res.status(BadRequest).json({ error: 'Content is required' });
+    return;
+  }
 
-	try {
-		const post = await prisma.post.create({
-			data: { content },
-		});
-		const success = 201;
-		res.status(success).json(post);
-	} catch (error) {
-		const internalServerError = 500;
-		res.status(internalServerError).json({ error });
-	}
+  try {
+    const post = await prisma.post.create({
+      data: { content },
+    });
+    const success = 201;
+    res.status(success).json(post);
+  } catch (error) {
+    const internalServerError = 500;
+    res.status(internalServerError).json({ error });
+  }
 });
 
-app.put("/posts/:id", async (req: Request, res: Response) => {
-	const { id } = req.params;
-	const content = req.body.content;
+app.put('/posts/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const content = req.body.content;
 
-	if (!content) {
-		const BadRequest = 400;
-		res.status(BadRequest).json({ error: "Content is required" });
-		return;
-	}
-  
-	try {
-		const updatedPost = await prisma.post.update({
-			where: { id: parseInt(id) },
-			data: { content },
-		});
-		res.json(updatedPost);
-	} catch (error) {
-		const internalServerError = 500;
-		console.error("Error updating post:", error);
-		res.status(internalServerError).json({ error: "Failed to update post" });
-	}
+  if (!content) {
+    const BadRequest = 400;
+    res.status(BadRequest).json({ error: 'Content is required' });
+    return;
+  }
+
+  try {
+    const updatedPost = await prisma.post.update({
+      where: { id: parseInt(id) },
+      data: { content },
+    });
+    res.json(updatedPost);
+  } catch (error) {
+    const internalServerError = 500;
+    console.error('Error updating post:', error);
+    res.status(internalServerError).json({ error: 'Failed to update post' });
+  }
 });
 
-app.delete("/posts/:id", async (req, res: Response) => {
-	const { id } = req.params;
-	try {
-		await prisma.post.delete({
-			where: { id: parseInt(id) },
-		});
-		const noContent = 500;
-		res.status(noContent).send();
-	} catch (error) {
-		const internalServerError = 500;
-		console.error("Error deleting post:", error);
-		res.status(internalServerError).json({ error: "Failed to delete post" });
-	}
+app.delete('/posts/:id', async (req, res: Response) => {
+  const { id } = req.params;
+  try {
+    await prisma.post.delete({
+      where: { id: parseInt(id) },
+    });
+    const noContent = 500;
+    res.status(noContent).send();
+  } catch (error) {
+    const internalServerError = 500;
+    console.error('Error deleting post:', error);
+    res.status(internalServerError).json({ error: 'Failed to delete post' });
+  }
 });
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`),
+);
